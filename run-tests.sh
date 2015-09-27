94440cf1bd91d38ebdfb0e29da008339c014fa8e
James Talmage <james@talmage.io>
2015-09-27 00:54:05 -0400

    add assertions to tests

--- run-tests.sh
+++ run-tests.sh
@@ -2,8 +2,18 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR
+export FALLBACK_CLI_FIXTURE_BASE=$DIR/test/fixture
+FB=$FALLBACK_CLI_FIXTURE_BASE
 
-cd test/fixture/module_a && ../cli_module/cli-shim.js && ./node_modules/cli_module/cli-shim.js
-cd ../module_b && ../cli_module/cli-shim.js && ./node_modules/cli_module/cli-shim.js
-cd ../module_c && ../cli_module/cli-shim.js
+cd $FB/module_a
+EXPECT_SHIM=global EXPECT_CLI=a ../cli_module/cli-shim.js
+cd $FB/module_a
+EXPECT_SHIM=a EXPECT_CLI=a ./node_modules/cli_module/cli-shim.js
+
+cd $FB/module_b
+EXPECT_SHIM=global EXPECT_CLI=b ../cli_module/cli-shim.js
+cd $FB/module_b
+EXPECT_SHIM=b EXPECT_CLI=b ./node_modules/cli_module/cli-shim.js
+
+cd $FB/module_c
+EXPECT_SHIM=global EXPECT_CLI=global ../cli_module/cli-shim.js
--- test/fixture/cli_module/cli-shim.js
+++ test/fixture/cli_module/cli-shim.js
@@ -1,5 +1,55 @@
 #!/usr/bin/env node
+var path = require('path');
+var assert = require('assert');
+var expectedShim = process.env.EXPECT_SHIM;
+var expectedCli = process.env.EXPECT_CLI;
+var fixtureBase = process.env.FALLBACK_CLI_FIXTURE_BASE;
+var expectedLocation = expectedCli === 'global' ? 'global' : 'local';
+var shimPath = relative(__filename);
+
+// Figure out if this shim is the expected one.
+if (expectedShim === 'global') {
+	assert(
+		/^cli_module[\/\\]cli-shim\.js$/
+			.test(shimPath),
+		'shimPath should be global: ' + shimPath
+	);
+} else {
+	assert.strictEqual(
+		/^module_(.)[\/\\].+?[\/\\]cli-shim\.js$/
+			.exec(shimPath)[1],
+		expectedShim,
+		shimPath
+	);
+}
+
+console.log(shimPath);
+
 require('fallback-cli')({
 	module: 'cli_module',
-	cliPath: './cli'
+	cliPath: './cli',
+	before: function (location, cliPath) {
+		console.log('before', location, relative(cliPath));
+		assert.strictEqual(location, expectedLocation);
+		return 'beforeResult';
+	},
+	run: function (location, cliModule, result) {
+		assert.strictEqual(location, expectedLocation);
+		assert.strictEqual(cliModule, expectedCli);
+		assert.strictEqual('beforeResult', result);
+		console.log('run', location, cliModule, result);
+		return 'runResult';
+	},
+	after: function (location, cliModule, result) {
+		assert.strictEqual(location, expectedLocation);
+		assert.strictEqual(cliModule, expectedCli);
+		assert.strictEqual('runResult', result);
+		console.log('after', location, cliModule, result);
+		console.log();
+		console.log();
+	}
 });
+
+function relative(p) {
+	return path.relative(fixtureBase, p);
+}
